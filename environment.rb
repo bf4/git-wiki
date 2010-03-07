db64f85f82e705b2362bbc9788351dbc0d2785aa
Ontje Helmich <ontje@me.com>
2010-03-07 19:31:34 +0100

    When creating a fresh repo, also add a default file and a first commit.

--- environment.rb
+++ environment.rb
@@ -7,14 +7,19 @@ require 'page'
 end
 
 GIT_REPO = File.expand_path( ENV['WIKI_HOME'] || (File.expand_path(File.dirname(__FILE__)) + '/deploy/var/repo') )
-HOMEPAGE = 'index'
-PAGE_FILE_EXT = ".page"
+HOMEPAGE = 'index' # default homepage for every directory
+PAGE_FILE_EXT = ".page" # file extension
 ATTACH_DIR_SUFFIX = "_files"
 HTML_EXT = '.html'
 
+# check if the repo already exists, if not create a repo, a default file and a first commit.
 unless File.exists?(GIT_REPO) && File.directory?(GIT_REPO)
   puts "Initializing repository in #{GIT_REPO}..."
   Git.init(GIT_REPO)
+  File.open(GIT_REPO + "/" + HOMEPAGE + PAGE_FILE_EXT, 'w') { |f| f.puts "Welcome to git-wiki!" } # write something to the default index file with default extension
+  g = Git.open(GIT_REPO)
+  g.add(HOMEPAGE + PAGE_FILE_EXT)
+  g.commit("Initial commit for automatically created git-wiki repo.")
 end
 
 $repo = Git.open(GIT_REPO)
